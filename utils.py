e80f65d12b4d6afdd082e5bf712467279b6239b9
pchenik <49047113+pchenik@users.noreply.github.com>
2020-05-28 23:21:45 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 
+
 def call_counter(func):
     def helper(*args, **kwargs):
         helper.calls += 1
         return func(*args, **kwargs)
     helper.calls = 0
     return helper
+
 
 
 def get_log_error(x, y, axis=1):
